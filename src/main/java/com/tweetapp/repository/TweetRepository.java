26297d5ad2bc11500ac31f4058cf033b7ea43f7d
Imgroot22 <yogesh.r3@cognizant.com>
2022-04-25 17:53:58 +0530

    added tweet repository

---
+++ src/main/java/com/tweetapp/repository/TweetRepository.java
@@ -0,0 +1,11 @@
+package com.tweetapp.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tweetapp.model.Tweet;
+
+@Repository
+public interface TweetRepository extends MongoRepository<Tweet,String> {
+
+}
