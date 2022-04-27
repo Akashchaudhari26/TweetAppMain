08cac287d931b56f268d80265f1aaf5659648f6b
Imgroot22 <yogesh.r3@cognizant.com>
2022-04-27 14:46:25 +0530

    added get tweets end point

--- src/main/java/com/tweetapp/controller/TweetController.java
+++ src/main/java/com/tweetapp/controller/TweetController.java
@@ -1,11 +1,14 @@
 package com.tweetapp.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -72,5 +75,11 @@ public class TweetController {
 		tweetReplyRequest.setTweetId(tweetId);
 		Tweet tweet = tweetService.replyTweet(tweetReplyRequest);
 		return new ResponseEntity<>(tweet, HttpStatus.OK);
+	}
+
+	@GetMapping("/all")
+	public ResponseEntity<List<Tweet>> getAllTweets() {
+		
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
