4ca6688e475f926fd09abe1c1b7db5bdad28d6f4
Imgroot22 <yogesh.r3@cognizant.com>
2022-05-01 11:42:44 +0530

    Enabled web security and mongo auditing

--- src/main/java/com/tweetapp/TweetAppApplication.java
+++ src/main/java/com/tweetapp/TweetAppApplication.java
@@ -2,8 +2,12 @@ package com.tweetapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity(debug = true)
+@EnableMongoAuditing
 public class TweetAppApplication {
 
 	public static void main(String[] args) {
