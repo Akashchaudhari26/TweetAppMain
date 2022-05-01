221e85f28afe1c9a7f159d33f68db48fde063d6e
Imgroot22 <yogesh.r3@cognizant.com>
2022-05-01 11:40:56 +0530

    added constants

---
+++ src/main/java/com/tweetapp/constants/SecurityConstants.java
@@ -0,0 +1,6 @@
+package com.tweetapp.constants;
+
+public class SecurityConstants {
+	public static final String JWT_KEY = "jxgEQeXHuPq8VdbyYFNkANdudQ53YUn4";
+	public static final String JWT_HEADER = "Authorization";
+}
