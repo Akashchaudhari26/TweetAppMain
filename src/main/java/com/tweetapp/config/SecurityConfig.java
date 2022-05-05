78fe89e3b13a3ea44783bad7042959ee59960e18
Imgroot22 <yogesh.r3@cognizant.com>
2022-05-05 11:39:03 +0530

    added new security config

--- src/main/java/com/tweetapp/config/SecurityConfig.java
+++ src/main/java/com/tweetapp/config/SecurityConfig.java
@@ -1,68 +1,62 @@
 package com.tweetapp.config;
 
-import java.util.Arrays;
-import java.util.Collections;
-
-import javax.servlet.http.HttpServletRequest;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.tweetapp.filters.JWTTokenGeneratorFilter;
-import com.tweetapp.filters.JWTTokenValidatorFilter;
-import com.tweetapp.filters.UserOperationValidatorFilter;
+import com.tweetapp.security.filters.AuthenticationTokenFilter;
+import com.tweetapp.security.service.UserDetailsServiceImp;
 
 @Configuration
+@EnableGlobalMethodSecurity(
+		prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	UserDetailsServiceImp userDetailsService;
+
+	@Autowired
+	private AuthenticationEntryPoint unauthorizedHandler;
+
+	@Override
+	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+		authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
+	
+	@Bean
+	public AuthenticationTokenFilter authenticationJwtTokenFilter() {
+		return new AuthenticationTokenFilter();
+	}
+	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
-		http.httpBasic();
-		
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().
-		cors().disable().csrf().disable()
-		.addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
-		.addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
-		.authorizeRequests()
-		.mvcMatchers(HttpMethod.GET, "/api/v1.0/tweets/all").authenticated()
-		.mvcMatchers(HttpMethod.GET,"/api/v1.0/tweets/login").authenticated();
-//				.addFilterAfter(new UserOperationValidatorFilter(), JWTTokenValidatorFilter.class)
-//				.authorizeRequests()
-//				.mvcMatchers(HttpMethod.GET,"/api/v1.0/tweets/login").permitAll()
-//				.mvcMatchers(HttpMethod.POST,"/api/v1.0/tweets/register").permitAll()
-//				.mvcMatchers(HttpMethod.GET,"/api/v1.0/tweets/{loginId:^[A-Za-z0-9_]+$}/forgot").permitAll()
-//				.anyRequest().permitAll().and().httpBasic();
+		http.cors().and().csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
+				.antMatchers("/api/v1.0/tweets/login").permitAll().antMatchers("/api/v1.0/tweets/register").permitAll()
+				.antMatchers("/api/v1.0/tweets/{loginId:^[A-Za-z0-9_]+$}/forgot").permitAll()
+				.anyRequest().authenticated();
 
-		
-		
-//		.configurationSource(new CorsConfigurationSource() {
-//			@Override
-//			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-//				CorsConfiguration config = new CorsConfiguration();
-//				config.setAllowedOrigins(Collections.singletonList("*"));
-//				config.setAllowedMethods(Collections.singletonList("*"));
-//				config.setAllowCredentials(true);
-//				config.setAllowedHeaders(Collections.singletonList("*"));
-//				config.setExposedHeaders(Arrays.asList("*"));
-//				config.setMaxAge(3600L);
-//				return config;
-//			}
-//		}).and()
-//		.csrf().disable()
+		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-//		return NoOpPasswordEncoder.getInstance();
 		return new BCryptPasswordEncoder();
 	}
+
 }
