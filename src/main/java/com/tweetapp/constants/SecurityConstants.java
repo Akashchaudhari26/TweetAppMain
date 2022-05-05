ef012232bda6894bc32d069eec8844d5a5a2e1ea
Imgroot22 <yogesh.r3@cognizant.com>
2022-05-05 11:38:09 +0530

    deleted old way constants

--- src/main/java/com/tweetapp/constants/SecurityConstants.java
+++
@@ -1,6 +0,0 @@
-package com.tweetapp.constants;
-
-public class SecurityConstants {
-	public static final String JWT_KEY = "jxgEQeXHuPq8VdbyYFNkANdudQ53YUn4";
-	public static final String JWT_HEADER = "Authorization";
-}
