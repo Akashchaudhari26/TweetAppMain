51bb626f7f64cb1946fcdfdcb4e4e0b083673320
Imgroot22 <yogesh.r3@cognizant.com>
2022-05-16 12:44:55 +0530

    unit-testing profile config

--- src/main/java/com/tweetapp/consumer/TweetConsumer.java
+++ src/main/java/com/tweetapp/consumer/TweetConsumer.java
@@ -2,6 +2,7 @@ package com.tweetapp.consumer;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
+@ConditionalOnProperty(value = "spring.kafka.enable", havingValue = "true", matchIfMissing = true)
 public class TweetConsumer {
 
 	@Autowired
